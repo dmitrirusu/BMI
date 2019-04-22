13c7d016d42b73fe90a26392441837b10d68b9c6
DmitriiRusu <dmitriirusu@gmail.com>
2019-04-22 14:53:15 +0300

    Added gradient toolbar

--- app/src/main/java/md/dmitrirusu/bmi/MainActivity.kt
+++ app/src/main/java/md/dmitrirusu/bmi/MainActivity.kt
@@ -1,11 +1,19 @@
 package md.dmitrirusu.bmi
 
-import android.support.v7.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val w = window
+            w.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+            w.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
