2954e0e6fcc7b14490868a827fc43cc9b99311bf
DmitriiRusu <dmitriirusu@gmail.com>
2019-04-23 14:39:50 +0300

    Adding admob

--- app/src/main/java/md/dmitrirusu/bmi/MainActivity.kt
+++ app/src/main/java/md/dmitrirusu/bmi/MainActivity.kt
@@ -4,14 +4,28 @@ import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.WindowManager
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdView: AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setStatusBarTransparent()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupAd()
+    }
+
+    private fun setupAd() {
+        MobileAds.initialize(this, getString(R.string.admob_app_id))
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().addTestDevice("CC9D944688B38D5B36C1FEFA577CBF9A").build()
+        mAdView.loadAd(adRequest)
     }
 
     private fun setStatusBarTransparent() {
