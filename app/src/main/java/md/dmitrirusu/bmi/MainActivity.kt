980fbe6a5b77d2ccfcc069bd88f83ca12202afab
DmitriiRusu <dmitriirusu@gmail.com>
2019-04-25 22:20:14 +0300

    Refactor layout to support different screen sizes

--- app/src/main/java/md/dmitrirusu/bmi/MainActivity.kt
+++ app/src/main/java/md/dmitrirusu/bmi/MainActivity.kt
@@ -2,12 +2,14 @@ package md.dmitrirusu.bmi
 
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.content.res.Resources
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.IBinder
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.WindowManager
+import android.view.*
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.InterstitialAd
@@ -27,6 +29,25 @@ class MainActivity : AppCompatActivity(),
     lateinit var gender: String
     var wiegth: Double = 0.0
     var height: Double = 0.0
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setStatusBarTransparent()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (hasNavigationBar()) {
+            setMargins(adView, /*Left*/0,/*Top*/ 0,/*Right*/0, /*Bottom*/dpToPx(48))
+        }
+
+        setupAdMob()
+        back_arrow.setOnClickListener { onBackPressed() }
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, MainFragment.newInstance())
+            .commit()
+    }
 
 
     override fun onContinueButtonClicked(userName: String, gender: String, wiegth: Double, height: Double) {
@@ -49,19 +70,14 @@ class MainActivity : AppCompatActivity(),
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        setStatusBarTransparent()
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        setupAdMob()
-        back_arrow.setOnClickListener { onBackPressed() }
-
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment_container, MainFragment.newInstance())
-            .commit()
+    private fun setMargins(view: View, left: Int, top: Int, right: Int, bottom: Int) {
+        if (view.layoutParams is ViewGroup.MarginLayoutParams) {
+            val p = view.layoutParams as ViewGroup.MarginLayoutParams
+            p.setMargins(left, top, right, bottom)
+            view.requestLayout()
+        }
     }
+
 
     private fun setupAdMob() {
         MobileAds.initialize(this, getString(R.string.admob_app_id))
@@ -98,6 +114,22 @@ class MainActivity : AppCompatActivity(),
         sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, getString(R.string.check_my_bmi_result))
         sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, getString(R.string.my_bmi_is, massIndex))
         startActivity(Intent.createChooser(sharingIntent, getString(R.string.where_to_send)))
+    }
+
+    private fun hasNavigationBar(): Boolean {
+        return try {
+            val serviceManager = Class.forName("android.os.ServiceManager")
+            val serviceBinder =
+                serviceManager.getMethod("getService", String::class.java).invoke(serviceManager, "window") as IBinder
+            val stub = Class.forName("android.view.IWindowManager\$Stub")
+            val windowManagerService = stub.getMethod("asInterface", IBinder::class.java).invoke(stub, serviceBinder)
+            val hasNavigationBar = windowManagerService.javaClass.getMethod("hasNavigationBar")
+            hasNavigationBar.invoke(windowManagerService) as Boolean
+        } catch (e: Exception) {
+            Log.w(this.javaClass.simpleName, "Couldn't determine whether the device has a navigation bar", e)
+            false
+        }
+
     }
 
     private fun calculateMassindex(): Double {
@@ -139,5 +171,21 @@ class MainActivity : AppCompatActivity(),
             window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
             window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
         }
+    }
+
+    fun hasNavBar(): Boolean {
+        var hasMenuKey = ViewConfiguration.get(this).hasPermanentMenuKey()
+        var hasBackKey = KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_BACK)
+
+
+        val id2 = resources.getIdentifier("config_enableTranslucentDecor", "bool", "android")
+        var b = id2 > 0 && resources.getBoolean(id2)
+
+        val id = resources.getIdentifier("config_showNavigationBar", "bool", "android")
+        return id > 0 && resources.getBoolean(id)
+    }
+
+    fun dpToPx(dp: Int): Int {
+        return (dp * Resources.getSystem().displayMetrics.density).toInt()
     }
 }
